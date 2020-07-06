b08a6e3def8dae3c4db079ecb5480f5cd4e0da17
HyejinYang <yang94lol@gmail.com>
2020-07-06 11:51:46 +0900

    Plus calculator

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,8 @@ class HomeController < ApplicationController
         @show_message = true
         @message = "도망쳐"
     end
+
+    def result
+        @plus_result = params[:num1].to_i + params[:num2].to_i
+    end
 end
