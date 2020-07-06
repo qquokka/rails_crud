162822c176987e63474abe00dc65f91204d4ac10
HyejinYang <yang94lol@gmail.com>
2020-07-06 16:02:13 +0900

    Create & Read

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
     def index
+        @posts = Post.all
     end
     
     def hi
@@ -15,6 +16,15 @@ class HomeController < ApplicationController
         @plus_result = params[:num1].to_i + params[:num2].to_i
     end
 
+    def write
+    end
+
     def create
+        post = Post.new
+        post.title = params[:title]
+        post.content = params[:content]
+        post.save
+
+        redirect_to '/index'
     end
 end
