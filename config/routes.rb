e45b0652b3824d14b9252d490ffa26937b2d849b
HyejinYang <yang94lol@gmail.com>
2020-07-06 10:17:41 +0900

    ADD hi controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # 주소로 처음 들어가면 HomeController에서 index 액션에 연결해주세요
   root 'home#index'  # get '/' => 'home#index' 라고도 쓸 수 있음
-
+  get '/home' => 'home#hi'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
